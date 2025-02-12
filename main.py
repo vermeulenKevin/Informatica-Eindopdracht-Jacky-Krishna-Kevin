6146cffcdd61f5f254f60aed0f3904ddd8c5960b
vermeulenKevin <kevinvermeulen0907@gmail.com>
2025-02-12 15:13:47 +0000

    afbeeldingen gebruikt ipv kleuren voor de grond en heg

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
 GRAS = (2, 212, 12)
 MUUR = (128, 128, 128)
-HEG = (12, 150, 12)
+HEG = (4, 135, 4)
 BLUE = (0, 0, 200)
 RED = (210, 0, 0)
 YELLOW = (230, 230, 0)
@@ -15,27 +15,17 @@ YELLOW = (230, 230, 0)
 SCREENW, SCREENH = pygame.display.Info().current_w, pygame.display.Info().current_h
 print(f"Screen Width: {SCREENW}, Screen Height: {SCREENH}")
 
-
 #SCREENW = 800
 #SCREENH = 800
 BOARDW = 768
 BOARDH = 768
 CELLW = BOARDW // 32
 CELLH = BOARDH // 32
-CENTER_CELLW = CELLW * 2
-CENTER_CELLH = CELLH * 2
 
-#grass_image = pygame.image.load("gras.png")
-#grass_image = pygame.transform.scale(grass_image, (CELLW, CELLH))
+print(f"Cell breedte: {CELLW} Cell hoogte: {CELLH}")
 
 pygame.display.set_caption (("Minotaurus")) 
-center_x = (SCREENW - CENTER_CELLW) // 2
-center_y = (SCREENH - CENTER_CELLH) // 2
 screen = pygame.display.set_mode((SCREENW, SCREENH), pygame.NOFRAME | pygame.FULLSCREEN)
-print(f"Center_x: {center_x}, Center_y: {center_y}")
-
-#def draw_board():
-    #pygame.draw.rect(screen, BLACK, (center_x,center_y, CENTER_CELLW, CENTER_CELLH))
 
 layout = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -53,8 +43,8 @@ layout = [
     [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
     [2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2],
     [2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 6, 6, 3, 3, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 6, 2, 2, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 2, 2, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 6, 7, 7, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 7, 7, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 4, 4, 5, 5, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2],
     [2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2],
     [1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
@@ -72,6 +62,16 @@ layout = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+
+gras_image = pygame.image.load("images/gras3.png")
+gras_image = pygame.transform.scale(gras_image, (CELLW, CELLH))
+
+heg_image = pygame.image.load("images/heg2.png")
+heg_image = pygame.transform.scale(heg_image, (CELLW, CELLH)) 
+
+muur_image = pygame.image.load("images/muur3.png")
+muur_image = pygame.transform.scale(muur_image, (CELLW, CELLH)) 
+
 def draw_board():
     for row in range(len(layout)):
         for col in range(len(layout[row])):
@@ -80,14 +80,8 @@ def draw_board():
             y = (SCREENH - BOARDH) // 2 + row * CELLH
 
             # Bepaal kleur op basis van de waarde in de matrix
-            if layout[row][col] == 0:
-                color = GRAS
-                #screen.blit(grass_image, (x, y))
-            elif layout[row][col] == 1:
-                color = HEG
-            elif layout[row][col] == 2:
-                color = MUUR
-            elif layout[row][col] == 3:
+            
+            if layout[row][col] == 3:
                 color = BLUE
             elif layout[row][col] == 4:
                 color = RED
@@ -95,12 +89,21 @@ def draw_board():
                 color = YELLOW
             elif layout[row][col] == 6:
                 color = WHITE
+            #elif layout[row][col] == 7:
+                #color = MIDDEN
 
             # Teken de tegel
-            pygame.draw.rect(screen, color, (x, y, CELLW, CELLH))
+            #pygame.draw.rect(screen, color, (x, y, CELLW, CELLH))
 
             # border om elke tile
             # pygame.draw.rect(screen, GRAY, (x, y, CELLW, CELLH), 1)
+
+            if layout[row][col] == 0:
+                screen.blit (gras_image, (x,y))
+            elif layout[row][col] == 1:
+                screen.blit (heg_image, (x,y))
+            elif layout[row][col] == 2:
+                screen.blit (muur_image, (x,y))
 
 
 
@@ -112,7 +115,8 @@ while running:
             running = False
 
     screen.fill(WHITE)
-    
+
+
     draw_board()
 
     pygame.display.update()
